055bb977d0defcfd7471de7d088010bd726dbe59
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-08-05 02:31:42 +0700

    Backup HomeFragment and ViewHolder

--- app/src/main/java/co/id/roningrum/dolanapptugasakhir/HomeFragment.java
+++ app/src/main/java/co/id/roningrum/dolanapptugasakhir/HomeFragment.java
@@ -14,8 +14,11 @@
 package co.id.roningrum.dolanapptugasakhir;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,16 +26,34 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import co.id.roningrum.dolanapptugasakhir.adapter_recomendation.FoodRecommendationViewHolder;
+import co.id.roningrum.dolanapptugasakhir.adapter_recomendation.TouristRecommendationViewHolder;
+import co.id.roningrum.dolanapptugasakhir.hotel.HotelActivity;
+import co.id.roningrum.dolanapptugasakhir.model.TourismItem;
+import co.id.roningrum.dolanapptugasakhir.tourism.nature.NatureCategoryActivity;
+import co.id.roningrum.dolanapptugasakhir.tourism.recreation.RecreationCategoryActivity;
+import co.id.roningrum.dolanapptugasakhir.tourism.shopping.ShoppingCategoryActivity;
+import co.id.roningrum.dolanapptugasakhir.tourism.village.VillageCategoryActivity;
+import co.id.roningrum.dolanapptugasakhir.transportation.airport.AirportActivity;
+import co.id.roningrum.dolanapptugasakhir.transportation.bus.BusActivity;
+import co.id.roningrum.dolanapptugasakhir.transportation.train.TrainActivity;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
-    private TextView tvAllCategory;
-    private LinearLayout natureMenu, entertainMenu, shoppingMenu, villageMenu,
-            airportMenu, hotelMenu, trainMenu, busMenu;
+public class HomeFragment extends Fragment implements View.OnClickListener {
     private RecyclerView rvTouristRecommendation, rvFoodRecommendation;
 
+    private FirebaseRecyclerAdapter<TourismItem, FoodRecommendationViewHolder> foodRecommendAdapter;
+    private FirebaseRecyclerAdapter<TourismItem, TouristRecommendationViewHolder> touristRecommendAdapter;
+    private DatabaseReference recommendationDB;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -43,7 +64,113 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView tvAllCategory = view.findViewById(R.id.tv_subtitle_category_main_menu);
+        LinearLayout natureMenu = view.findViewById(R.id.ln_alam_tour_home_menu);
+        LinearLayout entertainMenu = view.findViewById(R.id.ln_hiburan_tour_home_menu);
+        LinearLayout shoppingMenu = view.findViewById(R.id.ln_belanja_tour_home_menu);
+        LinearLayout villageMenu = view.findViewById(R.id.ln_desa_tour_home_menu);
+        LinearLayout airportMenu = view.findViewById(R.id.ln_bandara_public_home);
+        LinearLayout hotelMenu = view.findViewById(R.id.ln_hotel_public_home);
+        LinearLayout trainMenu = view.findViewById(R.id.ln_train_public_home);
+        LinearLayout busMenu = view.findViewById(R.id.ln_bus_public_home);
+
+        tvAllCategory.setOnClickListener(this);
+        natureMenu.setOnClickListener(this);
+        entertainMenu.setOnClickListener(this);
+        shoppingMenu.setOnClickListener(this);
+        villageMenu.setOnClickListener(this);
+        airportMenu.setOnClickListener(this);
+        hotelMenu.setOnClickListener(this);
+        trainMenu.setOnClickListener(this);
+        busMenu.setOnClickListener(this);
+
+        //rv
+        rvTouristRecommendation = view.findViewById(R.id.rv_tourism_recommendation);
+        rvFoodRecommendation = view.findViewById(R.id.rv_food_recommendation);
+
+        rvFoodRecommendation.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvTouristRecommendation.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recommendationDB = FirebaseDatabase.getInstance().getReference();
+        showFoodReccomendation();
+        showTourismRecommendation();
+        return view;
     }
 
+    private void showTourismRecommendation() {
+        Query tourismRecommendQuery = recommendationDB.child("Tourism").limitToFirst(5);
+        FirebaseRecyclerOptions<TourismItem> tourismOption = new FirebaseRecyclerOptions.Builder<TourismItem>()
+                .setQuery(tourismRecommendQuery, TourismItem.class)
+                .build();
+        touristRecommendAdapter = new FirebaseRecyclerAdapter<TourismItem, TouristRecommendationViewHolder>(tourismOption) {
+            @Override
+            protected void onBindViewHolder(@NonNull TouristRecommendationViewHolder holder, int position, @NonNull TourismItem model) {
+                holder.showRecommendationTourismData(model);
+            }
+
+            @NonNull
+            @Override
+            public TouristRecommendationViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                return new TouristRecommendationViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_tourism_recommendation_home, viewGroup, false));
+            }
+        };
+        touristRecommendAdapter.notifyDataSetChanged();
+        rvTouristRecommendation.setAdapter(touristRecommendAdapter);
+    }
+
+    private void showFoodReccomendation() {
+        Query foodRecommendedQuery = recommendationDB.orderByChild("category_tourism").equalTo("kuliner").limitToFirst(5);
+        FirebaseRecyclerOptions<TourismItem> foodOption = new FirebaseRecyclerOptions.Builder<TourismItem>()
+                .setQuery(foodRecommendedQuery, TourismItem.class)
+                .build();
+        foodRecommendAdapter = new FirebaseRecyclerAdapter<TourismItem, FoodRecommendationViewHolder>(foodOption) {
+            @Override
+            protected void onBindViewHolder(@NonNull FoodRecommendationViewHolder holder, int position, @NonNull TourismItem model) {
+                holder.showFoodRecommendationTourismData(model);
+            }
+
+            @NonNull
+            @Override
+            public FoodRecommendationViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                return new FoodRecommendationViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_food_recommendation_home, viewGroup, false));
+            }
+        };
+        foodRecommendAdapter.notifyDataSetChanged();
+        rvFoodRecommendation.setAdapter(foodRecommendAdapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_subtitle_category_main_menu:
+                startActivity(new Intent(getContext(), AllCategoryActivity.class));
+                break;
+            case R.id.ln_alam_tour_home_menu:
+                startActivity(new Intent(getContext(), NatureCategoryActivity.class));
+                break;
+            case R.id.ln_hiburan_tour_home_menu:
+                startActivity(new Intent(getContext(), RecreationCategoryActivity.class));
+                break;
+            case R.id.ln_belanja_tour_home_menu:
+                startActivity(new Intent(getContext(), ShoppingCategoryActivity.class));
+                break;
+            case R.id.ln_desa_tour_home_menu:
+                startActivity(new Intent(getContext(), VillageCategoryActivity.class));
+                break;
+            case R.id.ln_hotel_public_home:
+                startActivity(new Intent(getContext(), HotelActivity.class));
+                break;
+            case R.id.ln_bandara_public_home:
+                startActivity(new Intent(getContext(), AirportActivity.class));
+                break;
+            case R.id.ln_bus_public_home:
+                startActivity(new Intent(getContext(), BusActivity.class));
+                break;
+            case R.id.ln_train_public_home:
+                startActivity(new Intent(getContext(), TrainActivity.class));
+                break;
+        }
+
+    }
 }
