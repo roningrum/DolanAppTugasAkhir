997d79d2b5cb4aab0d84ddba74f55a24373d3ec8
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-12-18 10:57:26 +0700

    Update Project

--- app/src/main/java/co/id/roningrum/dolanapptugasakhir/ui/tourism/tourismMapActivity/TourismRekreasiMaps.java
+++ app/src/main/java/co/id/roningrum/dolanapptugasakhir/ui/tourism/tourismMapActivity/TourismRekreasiMaps.java
@@ -81,7 +81,7 @@ public class TourismRekreasiMaps extends FragmentActivity implements OnMapReadyC
                     double latNature = tourism.getLat_location_tourism();
                     double lngNature = tourism.getLng_location_tourism();
                     LatLng naturePlaceLoc = new LatLng(latNature, lngNature);
-                    recreationMap.moveCamera(CameraUpdateFactory.newLatLngZoom(naturePlaceLoc, 14.0f));
+                    recreationMap.moveCamera(CameraUpdateFactory.newLatLngZoom(naturePlaceLoc, 10.0f));
                     recreationMap.addMarker(new MarkerOptions().position(naturePlaceLoc).title(tourism.getName_tourism()).
                             icon(Utils.getBitmapDescriptor(getApplicationContext())).snippet(tourism.getLocation_tourism()));
                 }
